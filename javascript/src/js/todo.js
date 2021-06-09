32c9bb63e726da0d3b75140701d63907d21233de
kanta-akase <kanta-akase@gmo.jp>
2021-06-09 22:04:34 +0900

    initial commit

---
+++ javascript/src/js/todo.js
@@ -0,0 +1,29 @@
+"use strict"; //strictモード：開発者が安全に開発できるようになる
+
+const addTaskTarget = document.getElementsByClassName('js-addTask-target')[0];
+const addTaskValue = document.getElementsByClassName('js-addTask-value')[0];
+const addTaskTrigger = document.getElementsByClassName('js-addTask-trigger')[0];
+
+const removeTask = removeButton => {
+  const listItem = document.createElement('li');
+  addTaskTarget.removeChild(targetTask);
+};
+
+const addTask = task => {
+  const listItem = document.createElement('li');
+  const removeButton = document.createElement('button');
+
+  removeButton.innerText = '削除';
+  removeButton.addEventListener('click', () => removeTask(removeButton));
+
+  listItem.innerText = task;
+
+  listItem.append(removeButton);
+  addTaskTarget.appendChild(listItem);
+};
+
+addTaskTrigger.addEventListener('click', event => {
+  const task = addTaskValue.value;
+  addTask(task);
+  addTaskValue.value = '';
+});
